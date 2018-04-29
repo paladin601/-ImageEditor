397eab2d7890fa4d09594be692c3f6f1bdf96aa3
adrianjmejias <adrianjmejias@gmail.com>
2018-04-29 11:21:02 -0400

    nueva gui, guardar y cargar listo, no muestra imagen :(((((((, hay que buscar forma de hacerlo, sino hacemos display como los ejemplos del prepa y borralo

---
+++ src/IEv2/IEv2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IEv2;
+
+import javax.swing.JFrame;
+/**
+ *
+ * @author FliaMejias
+ */
+public class IEv2 extends JFrame{
+
+    public IEv2(){
+        IEv2UI UI = new IEv2UI();
+                
+        this.setResizable(false);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(1280, 680); //No Tocar
+        this.add(UI);
+        this.setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        IEv2 JA = new IEv2();
+    }
+    
+}
